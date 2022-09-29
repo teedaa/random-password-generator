d44bc2c6e97898b746d22cee84b311a71cbd3e2f
teedaa <pengtedachengllhs@gmail.com>
2022-09-28 21:33:01 -0700

    Add variables for password prompts

--- assets/script.js
+++ assets/script.js
@@ -13,8 +13,16 @@ function generatePassword(){
    var includeNumbers = confirm("Do you want to include numbers?");
    var includeSpecial = confirm("Do you want to include special characters?");
    var includeUpper = confirm("Do you want to include upper case?");
-   var includeLower = confirm("Do you want lower case?")
-}
+   var includeLower = confirm("Do you want lower case?");
+
+   var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+   var special = ["\\", "/", '\"', ".", "?", "!", "@", "#", "$", "%", "^," , "*", "(", ")", "-", "+"];
+   var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+   var special = ["\\", "/", '\"', ".", "?", "!", "@", "#", "$", "%", "^", "&" , "*", "(", ")", "-", "+"];
+   var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+   var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  
+  }
 
 
 function writePassword() {
