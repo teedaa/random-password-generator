2afd5b7d69c4350ae517ae81aabc34c72062e7f3
teedaa <pengtedachengllhs@gmail.com>
2022-09-28 22:27:59 -0700

    Add boolean values for prompts

--- assets/script.js
+++ assets/script.js
@@ -3,17 +3,18 @@ var generateBtn = document.querySelector("#generate");
 
 
 function generatePassword(){
+  password=""
   var userInput = prompt("How many characters do you want your password to be? Enter a number between 8-128.")
   var passwordLength = (userInput);
   if (passwordLength < 8 || passwordLength > 128){
     alert("Length is invalid");
-    return
+    return false;
 
   }
    var includeNumbers = confirm("Do you want to include numbers?");
    var includeSpecial = confirm("Do you want to include special characters?");
    var includeUpper = confirm("Do you want to include upper case?");
-   var includeLower = confirm("Do you want lower case?");
+   var includeLower = confirm("Do you want to include lower case?");
 
    var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
    var special = ["\\", "/", '\"', ".", "?", "!", "@", "#", "$", "%", "^," , "*", "(", ")", "-", "+"];
@@ -22,6 +23,20 @@ function generatePassword(){
    var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
    var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   
+   var choice = []
+   if (includeNumbers === true) {
+    choice.push(numbers);
+   }
+   if (includeSpecial === true) {
+    choice.push(special)
+   }
+   if (includeUpper === true) {
+    choice.push(upper)
+   }
+   if (includeLower === true) {
+    choice.push(lower)
+   }
+   console.log(choice)
   }
 
 
