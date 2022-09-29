a35bb636b975d0efef129e28798f6637afde6eb0
teedaa <pengtedachengllhs@gmail.com>
2022-09-28 21:14:00 -0700

    Add initial password prompt

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,31 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+function generatePassword(){
+  var userInput = prompt("How many characters do you want your password to be? Enter a number between 8-128.")
+  var passwordLength = (userInput);
+  if (passwordLength < 8 || passwordLength > 128){
+    alert("Length is invalid")
+
+  }
 
 }
 
-// Add event listener to generate button
+
+function writePassword() {
+  
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  
+  passwordText.value = password;
+  
+}
+
 generateBtn.addEventListener("click", writePassword);
+
+//Prompt for criteria
+//length, 8-128 char
+//character types, special, upper, lower, numbers
+// validate has at least 1 input
+//display password
