d14a0d206340f3659984d67a59410892f92a6367
teedaa <pengtedachengllhs@gmail.com>
2022-09-29 00:34:14 -0700

    Rework iteration for arrays

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var passwordInput = ""
+
 
 function generatePassword(){
   password=""
@@ -17,42 +17,33 @@ function generatePassword(){
    var includeUpper = confirm("Do you want to include upper case?");
    var includeLower = confirm("Do you want to include lower case?");
 
-   var numbers = ["0123456789"];
+   var numbers = ["0","1","2","3","4","5","6","7","8","9"];
    var special = ["\\", "/", '\"', ".", "?", "!", "@", "#", "$", "%", "^", "&" , "*", "-", "+"];
    var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
    var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   
    var choice = []
    if (includeNumbers === true) {
-    // choice.push(numbers);
-    choice = choice.concat(numbers);
+    choice.push(numbers);
    }
    if (includeSpecial === true) {
-    // choice.push(special);
-    choice += choice.concat(special);
+    choice.push(special);
    }
    if (includeUpper === true) {
-    // choice.push(upper);
-    choice += choice.concat(upper);
+    choice.push(upper);
    }
    if (includeLower === true) {
-    // choice.push(lower);
-    choice += choice.concat(lower);
-    console.log(choice);
-    // choice = choice.concat();
-
+    choice.push(lower);
+   }
+ 
+  var passwordInput = "";
     for (var i = 0; i < passwordLength; i++) {
-      passwordInput = passwordInput + choice.charAt(Math.floor(Math.random() * choice.length)) ;
+        outerIndex = Math.floor(Math.random() * choice.length);
+        innerIndex = Math.floor(Math.random() * choice[outerIndex].length);
+        passwordInput = passwordInput + choice[outerIndex][innerIndex];
     }
     return passwordInput;
-  }
 }
-  //   for (var i = 0; i <passwordLength.length; i++) { 
-  //      var innerChoice = passwordLength[i].length;     
-  //      for (var a = 0; a < innerChoice; a++) {
-  //     passwordInput = passwordInput + passwordLength[i][a][Math.floor(Math.random()) * innerChoice[i][a].length];}
-  //   }
-  //  }
 
 
 function writePassword() {
@@ -64,9 +55,3 @@ function writePassword() {
 }
 
 generateBtn.addEventListener("click", writePassword);
-
-//Prompt for criteria
-//length, 8-128 char
-//character types, special, upper, lower, numbers
-// validate has at least 1 input
-//display password
